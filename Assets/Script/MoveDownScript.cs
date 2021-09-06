dca97ce6369c14862be9a139b5304baa72b05a23
Okorie Chiagozie Christian <christian94444@gmail.com>
2021-09-06 11:25:38 +0100

    Add project files.

---
+++ Assets/Script/MoveDownScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDownScript : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody objectRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objectRigidbody.AddForce(-Vector3.forward * speed);
+    }
+    private void LateUpdate()
+    {
+        if(transform.position.z < -15)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Script/PlayerConto.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerConto : MonoBehaviour
+{
+    private float speed = 10f;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        rb.AddForce(Vector3.forward * speed * verticalInput);
+        rb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+    private void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+        if(transform.position.z > 10.67f)
+        {
+            pos.z = 10.67f;
+        }else if(transform.position.z < -12.33f)
+        {
+            pos.z = -12.33f;
+        }
+        transform.position = pos;
+    }
+}
